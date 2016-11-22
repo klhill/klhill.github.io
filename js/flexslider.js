7a9ee7b9cecd40406a6683d204265443fd6553cb
Kelly <kelly.lisabeth.hill@gmail.com>
2016-11-22 16:31:27 -0500

    case study slider

--- js/flexslider.js
+++ js/flexslider.js
@@ -3,3 +3,24 @@ $(window).load(function() {
     animation: "slide"
   });
 });
+
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  });
+});
