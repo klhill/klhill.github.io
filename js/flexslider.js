e95b29dee3f0b2026fa20094dab0c81d1146716c
Kelly <kelly.lisabeth.hill@gmail.com>
2016-11-23 11:00:40 -0500

    case studies

--- js/flexslider.js
+++ js/flexslider.js
@@ -11,18 +11,3 @@ $(window).load(function() {
     controlNav: "thumbnails"
   });
 });
-
-// carousel
-
-$('.bxslider').bxSlider({
-  buildPager: function(slideIndex){
-    switch(slideIndex){
-      case 0:
-        return '<img src="/images/thumbs/tree_root.jpg">';
-      case 1:
-        return '<img src="/images/thumbs/houses.jpg">';
-      case 2:
-        return '<img src="/images/thumbs/hill_fence.jpg">';
-    }
-  }
-});
